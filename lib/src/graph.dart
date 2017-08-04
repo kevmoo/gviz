35fe01d5b7eccdbef9eed7e9812ce231b2d4219c
Kevin Moore <kevmoo@google.com>
2017-08-04 09:12:34 -0700

    One top-level library

--- lib/src/graph.dart
+++ lib/src/graph.dart
@@ -1,3 +1,6 @@
+// Copyright (c) 2017, Kevin Moore. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
 class Graph {
   final _edges = new Set<_Edge>();
 
--- test/gviz_test.dart
+++ test/gviz_test.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2017, Kevin Moore. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:gviz/gviz.dart';
+import 'package:gviz/src/gviz.dart';
 import 'package:test/test.dart';
 
 void main() {
