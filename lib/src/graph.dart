7120a21a9c474daf9b6dadf17cdd0978b186afc3
Kevin Moore <kevmoo@google.com>
2017-08-04 14:19:11 -0700

    starting on flagging

--- lib/src/graph.dart
+++ lib/src/graph.dart
@@ -1,12 +1,112 @@
 // Copyright (c) 2017, Kevin Moore. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'dart:collection';
+
+import 'package:collection/collection.dart';
+
 import 'gviz.dart';
 
 class Graph {
-  final _edges = new Set<_Edge>();
+  final _edges = new Set<Edge>();
 
-  bool addEdge(Object a, Object b) => _edges.add(new _Edge(a, b));
+  Graph();
+
+  factory Graph.fromEdges(Iterable<List<Object>> edges) {
+    var graph = new Graph();
+
+    for (var e in edges) {
+      if (e.length != 2) {
+        throw new ArgumentError('All values in `edges` must have length `2`.');
+      }
+
+      graph.addEdge(e[0], e[1]);
+    }
+
+    return graph;
+  }
+
+  Set<Edge> get edges => new UnmodifiableSetView<Edge>(_edges);
+
+  bool addEdge(Object from, Object to) => _edges.add(new Edge(from, to));
+
+  Edge edgeFor(Object from, Object to) => _edges
+      .firstWhere((e) => e.from == from && e.to == to, orElse: () => null);
+
+  EdgeFlag flagEdge(Object from, Object to) => _flagEdge(from, to, null);
+
+  EdgeFlag _flagEdge(Object from, Object to, EdgeFlag flag) {
+    EdgeFlag gf() => flag ??= new EdgeFlag();
+
+    var queue = new Queue();
+
+    EdgeFlag flagHelper(Object from, Object to) {
+      assert(from != to, 'Not doing loops for now...');
+      assert(!queue.contains(to));
+
+      EdgeFlag helperFlag;
+
+      if (queue.contains(from)) {
+        // loop!
+        return null;
+      }
+      queue.add(from);
+
+      try {
+        for (var edge in _edges.where((e) => e.from == from)) {
+          if (flag != null && edge._flags.contains(flag)) {
+            // we've already been here! return the flag!
+            return flag;
+          }
+          if (edge.from == from && edge.to == to) {
+            // this had better be the first time, right?
+            flag = helperFlag = gf();
+            edge._add(helperFlag);
+          } else {
+            // must go deeper
+            var deepFlag = flagHelper(edge.to, to);
+            if (deepFlag != null) {
+              if (helperFlag == null) {
+                helperFlag = deepFlag;
+              }
+              assert(helperFlag == deepFlag);
+              assert(deepFlag == flag);
+              // There is a path from `edge.to` to the target `to`,
+              // so there is also a path from `edge.from` to `to`
+              // so add it!
+              edge._add(deepFlag);
+            }
+          }
+        }
+      } finally {
+        assert(queue.last == from);
+        queue.removeLast();
+      }
+
+      return helperFlag;
+    }
+
+    return flagHelper(from, to);
+  }
+
+  @Deprecated('not ready yet')
+  EdgeFlag flagEdges(Iterable from, Iterable to) {
+    EdgeFlag flag;
+
+    for (var fromItem in from) {
+      for (var toItem in to) {
+        var flagValue = _flagEdge(fromItem, toItem, flag);
+        if (flagValue != null) {
+          if (flag == null) {
+            flag = flagValue;
+          }
+          assert(flag == flagValue);
+        }
+      }
+    }
+
+    return flag;
+  }
 
   Gviz createGviz() {
     var gviz = new Gviz();
@@ -26,8 +126,8 @@ class Graph {
         });
 
     for (var edge in _edges) {
-      var idA = addNode(edge.a);
-      var idB = addNode(edge.b);
+      var idA = addNode(edge.from);
+      var idB = addNode(edge.to);
 
       gviz.addEdge(idA, idB);
     }
@@ -45,19 +145,38 @@ Iterable<String> _validIds(Object node) sync* {
   }
 }
 
-class _Edge {
-  final Object a;
-  final Object b;
+class EdgeFlag {
+  static int _count = 0;
+  final int _id;
 
-  _Edge(this.a, this.b);
+  EdgeFlag() : this._id = _count++;
+
+  @override
+  String toString() => 'EdgeFlag:$_id';
+}
+
+class Edge {
+  final Object from;
+  final Object to;
+
+  final _flags = new Set<EdgeFlag>();
+
+  Set<EdgeFlag> get flags => new UnmodifiableSetView(_flags);
+
+  Edge(this.from, this.to);
+
+  void _add(EdgeFlag flag) {
+    var added = _flags.add(flag);
+    assert(added, 'Should never add more than once.');
+  }
 
   @override
   bool operator ==(Object other) =>
-      other is _Edge && this.a == other.a && this.b == other.b;
+      other is Edge && this.from == other.from && this.to == other.to;
 
   @override
-  int get hashCode => a.hashCode ^ b.hashCode * 37;
+  int get hashCode => from.hashCode ^ to.hashCode * 37;
 
   @override
-  String toString() => 'Edge: `$a` -> `$b`';
+  String toString() => 'Edge: `$from` -> `$to`';
 }
--- test/graph_test.dart
+++ test/graph_test.dart
@@ -6,12 +6,159 @@ import 'package:test/test.dart';
 
 import 'test_util.dart';
 
+void printGraph(Graph g) {
+  for (var e in g.edges) {
+    print('$e\t${e.flags}');
+  }
+}
+
 void main() {
   test('duplicate edge returns false', () {
     var graph = new Graph();
 
     expect(graph.addEdge(1, 2), isTrue);
     expect(graph.addEdge(1, 2), isFalse);
+  });
+
+  group('flagEdges', () {
+    test('simple', () {
+      var graph = new Graph()..addEdge(1, 2);
+
+      var flag = graph.flagEdge(1, 2);
+
+      expect(flag, isNotNull);
+
+      expect(graph.edges, hasLength(1));
+      expect(graph.edges.single.flags.single, flag);
+    });
+
+    test('empty', () {
+      var graph = new Graph();
+
+      var flag = graph.flagEdge(1, 2);
+
+      expect(flag, isNull);
+
+      expect(graph.edges, isEmpty);
+    });
+
+    test('no match', () {
+      var graph = new Graph()..addEdge(1, 2);
+
+      var flag = graph.flagEdge(1, 3);
+
+      expect(flag, isNull);
+
+      expect(graph.edges, hasLength(1));
+      expect(graph.edges.single.flags, isEmpty);
+    });
+
+    test('1-2-3', () {
+      var graph = new Graph.fromEdges([
+        [1, 2],
+        [2, 3]
+      ]);
+      expect(graph.edges, hasLength(2));
+
+      var flag = graph.flagEdge(1, 3);
+
+      expect(flag, isNotNull);
+      expect(graph.edgeFor(1, 2).flags.single, flag);
+      expect(graph.edgeFor(2, 3).flags.single, flag);
+    });
+
+    test('1-2-3-1', () {
+      var graph = new Graph.fromEdges([
+        [1, 2],
+        [2, 3],
+        [3, 1]
+      ]);
+      expect(graph.edges, hasLength(3));
+
+      var flag = graph.flagEdge(1, 3);
+
+      expect(flag, isNotNull);
+      expect(graph.edgeFor(1, 2).flags.single, flag);
+      expect(graph.edgeFor(2, 3).flags.single, flag);
+      expect(graph.edgeFor(3, 1).flags, isEmpty);
+    });
+
+    test('diamond', () {
+      var graph = new Graph.fromEdges([
+        [1, 2],
+        [2, 4],
+        [1, 3],
+        [3, 4],
+        [4, 5], // not interesting
+        [0, 1], // not interesting
+        [1, 5], // not interesting
+      ]);
+      expect(graph.edges, hasLength(7));
+
+      var flag = graph.flagEdge(1, 4);
+
+      expect(flag, isNotNull);
+
+      expect(graph.edgeFor(1, 2).flags.single, flag);
+      expect(graph.edgeFor(2, 4).flags.single, flag);
+      expect(graph.edgeFor(1, 3).flags.single, flag);
+      expect(graph.edgeFor(3, 4).flags.single, flag);
+      expect(graph.edgeFor(4, 5).flags, isEmpty);
+      expect(graph.edgeFor(0, 1).flags, isEmpty);
+      expect(graph.edgeFor(1, 5).flags, isEmpty);
+    });
+
+    test('diamond one deeper', () {
+      var graph = new Graph.fromEdges([
+        [1, 2],
+        [2, 4],
+        [1, 3],
+        [3, 4],
+        [4, 5],
+        [0, 1], // not interesting
+        [1, 5],
+      ]);
+      expect(graph.edges, hasLength(7));
+
+      var flag = graph.flagEdge(1, 5);
+
+      expect(flag, isNotNull);
+
+      expect(graph.edgeFor(1, 2).flags.single, flag);
+      expect(graph.edgeFor(2, 4).flags.single, flag);
+      expect(graph.edgeFor(1, 3).flags.single, flag);
+      expect(graph.edgeFor(3, 4).flags.single, flag);
+      expect(graph.edgeFor(4, 5).flags.single, flag);
+      expect(graph.edgeFor(0, 1).flags, isEmpty);
+      expect(graph.edgeFor(1, 5).flags.single, flag);
+    });
+
+    test('diamond one deeper, plus loop', () {
+      var graph = new Graph.fromEdges([
+        [1, 2],
+        [2, 4],
+        [1, 3],
+        [3, 4],
+        [4, 5],
+        [0, 1], // not interesting
+        [1, 5],
+        [5, 1] // loop - not interesting
+      ]);
+      expect(graph.edges, hasLength(8));
+
+      var flag = graph.flagEdge(1, 5);
+
+      expect(flag, isNotNull);
+
+      expect(graph.edgeFor(1, 2).flags.single, flag);
+      expect(graph.edgeFor(2, 4).flags.single, flag);
+      expect(graph.edgeFor(1, 3).flags.single, flag);
+      expect(graph.edgeFor(3, 4).flags.single, flag);
+      expect(graph.edgeFor(4, 5).flags.single, flag);
+      expect(graph.edgeFor(0, 1).flags, isEmpty);
+      expect(graph.edgeFor(1, 5).flags.single, flag);
+      expect(graph.edgeFor(5, 1).flags, isEmpty);
+    });
   });
 
   group('createGviz', () {
