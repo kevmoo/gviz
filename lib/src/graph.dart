3829097c6351bf66453f82f4ad9af438308641a3
Kevin Moore <kevmoo@google.com>
2017-08-04 10:07:37 -0700

    Graph learned 'createGviz'

--- lib/src/graph.dart
+++ lib/src/graph.dart
@@ -1,10 +1,48 @@
 // Copyright (c) 2017, Kevin Moore. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'gviz.dart';
+
 class Graph {
   final _edges = new Set<_Edge>();
 
   bool addEdge(Object a, Object b) => _edges.add(new _Edge(a, b));
+
+  Gviz createGviz() {
+    var gviz = new Gviz();
+
+    var nodeIds = <Object, String>{};
+
+    String addNode(Object node) => nodeIds.putIfAbsent(node, () {
+          for (var option in _validIds(node)) {
+            if (!nodeIds.values.contains(option)) {
+              gviz.addNode(option, properties: {'label': node.toString()});
+              return option;
+            }
+          }
+
+          throw new UnsupportedError(
+              'Need to figure out another ID for `$node`, I guess...');
+        });
+
+    for (var edge in _edges) {
+      var idA = addNode(edge.a);
+      var idB = addNode(edge.b);
+
+      gviz.addEdge(idA, idB);
+    }
+
+    return gviz;
+  }
+}
+
+Iterable<String> _validIds(Object node) sync* {
+  yield node.toString();
+
+  var start = 1;
+  while (true) {
+    yield '${node}_${(start++).toString().padLeft(2, '0')}';
+  }
 }
 
 class _Edge {
--- test/graph_test.dart
+++ test/graph_test.dart
@@ -4,11 +4,56 @@
 import 'package:gviz/src/graph.dart';
 import 'package:test/test.dart';
 
+import 'test_util.dart';
+
 void main() {
   test('duplicate edge returns false', () {
     var graph = new Graph();
 
     expect(graph.addEdge(1, 2), isTrue);
     expect(graph.addEdge(1, 2), isFalse);
+  });
+
+  group('createGviz', () {
+    test('simple', () {
+      var graph = new Graph()..addEdge(1, 2)..addEdge(1, 3)..addEdge(2, 3);
+
+      var gviz = graph.createGviz();
+
+      gExpect(gviz, r'''digraph the_graph {
+  "1" [label="1"];
+  "2" [label="2"];
+  "1" -> "2";
+  "3" [label="3"];
+  "1" -> "3";
+  "2" -> "3";
+}
+''');
+    });
+
+    test('name collisions', () {
+      var graph = new Graph()
+        ..addEdge(1, 2)
+        ..addEdge(1, 3)
+        ..addEdge(2, 3)
+        ..addEdge('1', 1)
+        ..addEdge('2', '1');
+
+      var gviz = graph.createGviz();
+
+      gExpect(gviz, r'''digraph the_graph {
+  "1" [label="1"];
+  "2" [label="2"];
+  "1" -> "2";
+  "3" [label="3"];
+  "1" -> "3";
+  "2" -> "3";
+  "1_01" [label="1"];
+  "1_01" -> "1";
+  "2_01" [label="2"];
+  "2_01" -> "1_01";
+}
+''');
+    });
   });
 }
