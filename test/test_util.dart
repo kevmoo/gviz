387cb87c1e90de0465e9ce5fb76991e5df08e7a3
Kevin Moore <kevmoo@google.com>
2020-02-07 12:11:13 -0800

    Remove copyright headers in some files

--- test/test_util.dart
+++ test/test_util.dart
@@ -1,6 +1,3 @@
-// Copyright (c) 2017, Kevin Moore. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
 import 'package:gviz/gviz.dart';
 import 'package:test/test.dart';
 
